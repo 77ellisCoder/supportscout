939e8b8e4d0b3362e09d3ab34d673c595720c8f4
Christopher Ellis <77.ellis.christopher@gmail.com>
2026-09-10 12:14:42 +0800

    feat: refactor repository exports for device and web modules

--- repositories/Repository.ts
+++ repositories/Repository.ts
@@ -1,71 +1,21 @@
 import { Platform } from "react-native";
 
-import {
-    BandRepository as DeviceBandRepository,
-} from "./device/BandRepository";
-import {
-    GenreRepository as DeviceGenreRepository,
-} from "./device/GenreRepository";
-import {
-    GigRepository as DeviceGigRepository,
-} from "./device/GigRepository";
-import {
-    VenueRepository as DeviceVenueRepository,
-} from "./device/VenueRepository";
-import {
-    PrCampaignRepository as DevicePrCampaignRepository
-} from "./device/PrCampaignRepository"
-import {
-    PrContactRepository as DevicePrContactRepository
-} from "./device/PrContactRepository"
+import * as DeviceRepositories
+    from "./device";
 
-import {
-    BandRepository as WebBandRepository,
-} from "./web/BandRepository";
-import {
-    GenreRepository as WebGenreRepository,
-} from "./web/GenreRepository";
-import {
-    GigRepository as WebGigRepository,
-} from "./web/GigRepository";
-import {
-    VenueRepository as WebVenueRepository,
-} from "./web/VenueRepository";
-import {
-    PrCampaignRepository as WebPrCampaignRepository
-} from "./web/PrCampaignRepository"
-import {
-    PrContactRepository as WebPrContactRepository
-} from "./web/PrContactRepository"
+import * as WebRepositories
+    from "./web";
 
-const isWeb = Platform.OS === "web";
+const repositories =
+    Platform.OS === "web"
+        ? WebRepositories
+        : DeviceRepositories;
 
-export const BandRepository =
-    isWeb
-        ? WebBandRepository
-        : DeviceBandRepository;
-
-export const GenreRepository =
-    isWeb
-        ? WebGenreRepository
-        : DeviceGenreRepository;
-
-export const GigRepository =
-    isWeb
-        ? WebGigRepository
-        : DeviceGigRepository;
-
-export const PrCampaignRepository =
-    isWeb
-        ? WebPrCampaignRepository
-        : DevicePrCampaignRepository
-
-export const PrContactRepository =
-    isWeb
-        ? WebPrContactRepository
-        : DevicePrContactRepository
-
-export const VenueRepository =
-    isWeb
-        ? WebVenueRepository
-        : DeviceVenueRepository;
+export const {
+    BandRepository,
+    GenreRepository,
+    GigRepository,
+    VenueRepository,
+    PrCampaignRepository,
+    PrContactRepository,
+} = repositories;
---
+++ repositories/device/index.ts
@@ -0,0 +1,27 @@
+export {
+    BandRepository,
+} from "./BandRepository";
+
+export {
+    DrinkTokenRepository,
+} from "./DrinkTokenRepository"
+
+export {
+    GenreRepository,
+} from "./GenreRepository";
+
+export {
+    GigRepository,
+} from "./GigRepository";
+
+export {
+    VenueRepository,
+} from "./VenueRepository";
+
+export {
+    PrCampaignRepository,
+} from "./PrCampaignRepository";
+
+export {
+    PrContactRepository,
+} from "./PrContactRepository";
